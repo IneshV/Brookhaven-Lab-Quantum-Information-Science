09bb11634713c4c3bdad89ec5d4d417c11e99a91
Inesh V <ivytheswaran@wesleyan.edu>
2025-06-23 10:13:22 -0400

    Fully remove qis101 submodule

--- qis101/README.md
+++
@@ -1,2 +0,0 @@
-# Foundations of Quantum Information Science (QIS101)
-## Using VSCode, JupyterLab, and IBM Qiskit
--- qis101/labs/Session 02 - Installing Courseware/my_quip.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-"""my_quip.py"""
-print("Inesh Vytheswaran.")
-
-print("a witty short phrase")
